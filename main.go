8046c96afb77cd594415e277a37502a06f3012bc
TomasBorquez <tomasborquez13@gmail.com>
2024-08-07 13:49:22 -0300

    Remove documentation since its on example_test already

--- main.go
+++ main.go
@@ -14,40 +14,24 @@ var (
 
 // Error displays a message in a red color
 // Works like Printf, so you can pass arguments, so they are added to the string
-//
-// Example:
-//
-//	logger.Error("An error occurred: %s", err)
 func Error(message string, args ...any) {
 	fmt.Printf(Red+message+Reset+"\n", args...)
 }
 
 // Success displays a message in a green color
 // Works like Printf, so you can pass arguments, so they are added to the string
-//
-// Example:
-//
-//	logger.Success("Operation completed successfully: %d items processed", count)
 func Success(message string, args ...any) {
 	fmt.Printf(Green+message+Reset+"\n", args...)
 }
 
 // Warning displays a message in an orange color
 // Works like Printf, so you can pass arguments, so they are added to the string
-//
-// Example:
-//
-//	logger.Warning("Low disk space: %d%% remaining", percentage)
 func Warning(message string, args ...any) {
 	fmt.Printf(Orange+message+Reset+"\n", args...)
 }
 
 // Custom displays a simple message where you add the colors
 // Works like Printf, so you can pass arguments, so they are added to the string
-//
-// Example:
-//
-//	logger.Custom(logger.Blue + "Processing item: " + logger.Reset + "%s", itemName)
 func Custom(message string, args ...any) {
 	fmt.Printf(message+"\n", args...)
 }
