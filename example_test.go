eb5e169833565b08c5c4107255bd010537c80227
TomasBorquez <tomasborquez13@gmail.com>
2024-08-09 06:34:53 -0300

    Add background colors

--- example_test.go
+++ example_test.go
@@ -4,6 +4,11 @@ import (
 	"github.com/TomasBorquez/logger"
 )
 
+func ExampleInfo() {
+	logger.Info("Just some information being displayed %s", "here")
+	// Output: Just some information being displayed
+}
+
 func ExampleError() {
 	logger.Error("An error occurred: %s", "file not found")
 	// Output: An error occurred: file not found
--- main.go
+++ main.go
@@ -5,12 +5,30 @@ import (
 )
 
 var (
-	Reset  = "\033[0m"
-	Red    = "\033[0;31m"
-	Green  = "\033[0;32m"
-	Orange = "\033[0;33m"
-	Blue   = "\033[0;34m"
+	Reset    = "\033[0m"
+	Gray     = "\033[0;30m"
+	Red      = "\033[0;31m"
+	Green    = "\033[0;32m"
+	Orange   = "\033[0;33m"
+	Blue     = "\033[0;34m"
+	Purple   = "\033[0;35m"
+	Cyan     = "\033[0;36m"
+	White    = "\033[0;37m"
+	BgGray   = "\033[1;30m"
+	BgRed    = "\033[1;31m"
+	BgGreen  = "\033[1;32m"
+	BgOrange = "\033[1;33m"
+	BgBlue   = "\033[1;34m"
+	BgPurple = "\033[1;35m"
+	BgCyan   = "\033[1;36m"
+	BgWhite  = "\033[1;37m"
 )
+
+// Info displays a message in a gray color
+// Works like Printf, so you can pass arguments, so they are added to the string
+func Info(message string, args ...any) {
+	fmt.Printf(Gray+message+"\n", args...)
+}
 
 // Error displays a message in a red color
 // Works like Printf, so you can pass arguments, so they are added to the string
